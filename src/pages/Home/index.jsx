250a947430761e18d559cf70afc91696098ba0a2
Thanh Tung Nguyen <tungnt267@gmail.com>
2020-12-24 19:52:59 +0700

    add FilmBlock

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Carousel from "../../components/Carousel";
+import FilmBlock from "../../components/FilmBlock";
 import Header from "../../components/Header";
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
     <div>
       <Header />
       <Carousel />
+      <FilmBlock />
     </div>
   );
 };
