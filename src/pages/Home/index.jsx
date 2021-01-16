e876120bc70dc71c0d729f49d7a29b3de65c7e0c
Thanh Tung Nguyen <tungnt267@gmail.com>
2021-01-16 09:39:09 +0700

    update

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 // import Ads from "../../components/Ads";
 import Carousel from "../../components/Carousel";
-// import FilmBlock from "../../components/FilmBlock";
+import FilmBlock from "../../components/FilmBlock";
 import Header from "../../components/Header";
-import MovieTime from "../../components/MovieTime";
+// import MovieTime from "../../components/MovieTime";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <Carousel />
-      {/* <FilmBlock /> */}
-      <MovieTime />
+      <FilmBlock />
+      {/* <MovieTime /> */}
       {/* <Ads /> */}
     </div>
   );
